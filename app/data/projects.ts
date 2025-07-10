7cf4b3dc0e680788924ea1fe6871622dde2041dc
skrodrigo <rodrigoa0987@gmail.com>
2025-07-10 20:35:46 -0300

    feat add tech

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,4 +1,4 @@
-import { Project } from "@/app/types/interfaces";
+import type { Project } from '@/app/types/interfaces'
 
 export const projects: Project[] = [
   {
@@ -7,7 +7,6 @@ export const projects: Project[] = [
       'Sintesy ouve suas ideias, reuniões, planejamentos e cria uma linha contínua de raciocinio com anotações, tópicos, checklists e muito mais! Optimizando em 10x seus fluxos de trabalho.',
     image: '/sintesy.png',
     LandingPage: 'https://sintesy.me/',
-    Dashboard: 'https://dashboard.sintesy.me/',
     linkGithub: 'https://github.com/skrodrigo',
     technologies: ['Next.js', 'TypeScript', 'Tailwind'],
   },
@@ -17,6 +16,31 @@ export const projects: Project[] = [
       'Genesis é um SaaS de geração de interfaces Front End usando inteligência artificial.',
     image: '/genesis.png',
     linkGithub: 'https://github.com/skrodrigo/genesis',
-    technologies: ['Next.js', 'Prisma', 'Shadcn', 'BetterAuth', 'Tailwind CSS', 'Shadcn UI'],
+    technologies: [
+      'Next.js',
+      'Prisma',
+      'Shadcn',
+      'BetterAuth',
+      'Tailwind CSS',
+      'Shadcn UI',
+    ],
+  },
+  {
+    title: 'Receitai',
+    description:
+      'receitai é um SaaS de receitas desbloquadas por créditos e também gera receitas com AI.',
+    image: '/recipeai.png',
+    LandingPage: 'https://receitai.vercel.app/',
+    linkGithub: 'https://github.com/skrodrigo/receitai-microsaas-nextjs',
+    technologies: [
+      'Next.js',
+      'Prisma',
+      'Shadcn',
+      'BetterAuth',
+      'Tailwind CSS',
+      'Shadcn UI',
+      'NeonDB',
+      'Cakto Pay',
+    ],
   },
 ]
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Badge } from '@/components/ui/badge'
-import {
-  CardTitle
-} from '@/components/ui/card'
+import { CardTitle } from '@/components/ui/card'
 import {
   ArrowTopRightIcon,
   EnvelopeClosedIcon,
@@ -33,18 +31,30 @@ export default function Component() {
                 <AvatarImage src="/perfil.png" alt="Rodrigo Carvalho" />
                 <AvatarFallback>RC</AvatarFallback>
               </Avatar>
-              <div className='ml-0 flex justify-center flex-col items-center md:items-start md:-ml-14'>
+              <div className="ml-0 flex justify-center flex-col items-center md:items-start md:-ml-14">
                 <CardTitle className="text-xl">Rodrigo Carvalho</CardTitle>
                 <p className="text-sm text-zinc-400">Desenvolvedor Junior</p>
               </div>
-              <div className='flex justify-center md:justify-start md:-ml-[132px] ml-0 items-center md:items-start '>
-                <Link href="https://www.linkedin.com/in/skrodrigo" target="_blank" rel="noopener noreferrer">
+              <div className="flex justify-center md:justify-start md:-ml-[132px] ml-0 items-center md:items-start ">
+                <Link
+                  href="https://www.linkedin.com/in/skrodrigo"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <LinkedInLogoIcon className="h-5 w-5 mr-2" />
                 </Link>
-                <Link href="https://github.com/skrodrigo" target="_blank" rel="noopener noreferrer">
+                <Link
+                  href="https://github.com/skrodrigo"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <GitHubLogoIcon className="h-5 w-5 mr-2" />
                 </Link>
-                <Link href="https://www.instagram.com/skrodrigo" target="_blank" rel="noopener noreferrer">
+                <Link
+                  href="https://www.instagram.com/skrodrigo"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <EnvelopeClosedIcon className="h-5 w-5 mr-2" />
                 </Link>
               </div>
@@ -82,63 +92,68 @@ export default function Component() {
             animate={{ y: 0, opacity: 1 }}
             transition={{ delay: 0.2 }}
           >
-            <h2 className="text-xl font-semibold mb-4 text-white">Experiência</h2>
+            <h2 className="text-xl font-semibold mb-4 text-white">
+              Experiência
+            </h2>
             <div className="space-y-4 mb-4">
               <div className="border-b border-zinc-900 pb-4 space-y-1">
-                <span className='text-zinc-400 text-xs'>12/2024 - Now</span>
+                <span className="text-zinc-400 text-xs">12/2024 - Now</span>
                 <h3 className="font-medium text-zinc-100">
                   CEO & Co-Founder @ Sintesy
                 </h3>
                 <p className="text-sm text-zinc-400 max-w-2xl">
-                  Sintesy, um aplicativo capaz de transcrever e resumir: aulas, palestras, vídeos e criar anotações para você nunca esquecer qualquer conteúdo.
+                  Sintesy, um aplicativo capaz de transcrever e resumir: aulas,
+                  palestras, vídeos e criar anotações para você nunca esquecer
+                  qualquer conteúdo.
                   <br />
                   <br />
-                  Responsável pelo Desenvolvimento Web da plataforma, utilizando React, Next.js, Tailwind CSS, Context API e Axios, com foco em performance, escalabilidade e experiência do usuário.
+                  Responsável pelo Desenvolvimento Web da plataforma, utilizando
+                  React, Next.js, Tailwind CSS, Context API e Axios, com foco em
+                  performance, escalabilidade e experiência do usuário.
                   <br />
                   <br />
-                  Além do desenvolvimento técnico, liderei iniciativas de marketing estratégico e posicionamento digital, contribuindo significativamente para a expansão da base de usuários, impactando mais de 1.500 leads com a solução. Participando também ativamente de um programa de aceleração de startups no SebraeLAB, fortalecendo a evolução do produto e do negócio.
+                  Além do desenvolvimento técnico, liderei iniciativas de
+                  marketing estratégico e posicionamento digital, contribuindo
+                  significativamente para a expansão da base de usuários,
+                  impactando mais de 1.500 leads com a solução. Participando
+                  também ativamente de um programa de aceleração de startups no
+                  SebraeLAB, fortalecendo a evolução do produto e do negócio.
                 </p>
-                <div className='space-x-2'>
-                  <Badge variant='secondary'>
-                    Node.js
-                  </Badge>
-                  <Badge variant='secondary'>
-                    Python
-                  </Badge>
-                  <Badge variant='secondary'>
-                    Marketing
-                  </Badge>
-                  <Badge variant='secondary'>
-                    Liderança
-                  </Badge>
+                <div className="space-x-2">
+                  <Badge variant="secondary">Node.js</Badge>
+                  <Badge variant="secondary">Python</Badge>
+                  <Badge variant="secondary">Marketing</Badge>
+                  <Badge variant="secondary">Liderança</Badge>
                 </div>
               </div>
             </div>
             <div className="space-y-4">
               <div className="border-b border-zinc-900 pb-4 space-y-1">
-                <span className='text-zinc-400 text-xs'>12/2024 - 5/2025</span>
+                <span className="text-zinc-400 text-xs">12/2024 - 5/2025</span>
                 <h3 className="font-medium text-zinc-100">
                   Artificial Intelligence with AWS @ Compass UOL
                 </h3>
                 <p className="text-sm text-zinc-400 max-w-2xl">
-                  Durante meu estágio na trilha de Inteligência Artificial com AWS, desenvolvi chatbots inteligentes utilizando Python e Node.js, integrando serviços como AWS Lambda, EC2, S3, Textract, Bedrock, API Gateway e CloudWatch Logs. Estruturei arquiteturas serverless com foco em alta disponibilidade, escalabilidade automática e observabilidade em tempo real.
+                  Durante meu estágio na trilha de Inteligência Artificial com
+                  AWS, desenvolvi chatbots inteligentes utilizando Python e
+                  Node.js, integrando serviços como AWS Lambda, EC2, S3,
+                  Textract, Bedrock, API Gateway e CloudWatch Logs. Estruturei
+                  arquiteturas serverless com foco em alta disponibilidade,
+                  escalabilidade automática e observabilidade em tempo real.
                   <br />
                   <br />
-                  Implementei pipelines com Docker para ambientes conteinerizados, orquestrei instâncias EC2 e criei imagens otimizadas para produção. Participei ativamente de sprints ágeis com Scrum e Kanban, contribuindo em equipe para entregar soluções robustas, funcionais e alinhadas às melhores práticas de arquitetura na nuvem.
+                  Implementei pipelines com Docker para ambientes
+                  conteinerizados, orquestrei instâncias EC2 e criei imagens
+                  otimizadas para produção. Participei ativamente de sprints
+                  ágeis com Scrum e Kanban, contribuindo em equipe para entregar
+                  soluções robustas, funcionais e alinhadas às melhores práticas
+                  de arquitetura na nuvem.
                 </p>
-                <div className='space-x-2'>
-                  <Badge variant='secondary'>
-                    Node.js
-                  </Badge>
-                  <Badge variant='secondary'>
-                    Python
-                  </Badge>
-                  <Badge variant='secondary'>
-                    AI/ML
-                  </Badge>
-                  <Badge variant='secondary'>
-                    AWS
-                  </Badge>
+                <div className="space-x-2">
+                  <Badge variant="secondary">Node.js</Badge>
+                  <Badge variant="secondary">Python</Badge>
+                  <Badge variant="secondary">AI/ML</Badge>
+                  <Badge variant="secondary">AWS</Badge>
                 </div>
               </div>
             </div>
@@ -168,7 +183,7 @@ export default function Component() {
             transition={{ delay: 0.4 }}
           >
             <h2 className="text-xl font-semibold mb-4">Projetos</h2>
-            <div className="flex-col sm:flex-row flex gap-4">
+            <div className="flex flex-col xl:flex-row gap-4 justify-center xl:justify-start">
               {projects.map((project, index) => (
                 <motion.div
                   key={project.title}
@@ -186,7 +201,10 @@ export default function Component() {
             animate={{ y: 0, opacity: 1 }}
             transition={{ delay: 0.3 }}
           >
-            <Link href="/blog" className="group transition-colors flex items-center">
+            <Link
+              href="/blog"
+              className="group transition-colors flex items-center"
+            >
               <h2 className="text-xl font-semibold hover:opacity-90">Blog</h2>
               <ArrowTopRightIcon className="h-3 w-3 ml-1 group-hover:-translate-y-1 group-hover:translate-x-1 transition-transform" />
             </Link>
